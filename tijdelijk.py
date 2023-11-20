55325a14451766f32dae94140307efd05f0af678
Niels van den Hoven <nielsvdhoven@gmail.com>
2023-11-20 22:02:42 +0100

    Fucties toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -6,17 +6,18 @@ def print_aanbieding() :
         "vanille" : 4,
         "chocolade" : 5
     }
+    
     aanbieding = prijzen["aardbei"] * 0.8
-reclame_tekst= f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
-reclame_tekst2= reclame_tekst[:63]
-reclame_tekst3= reclame_tekst2 .upper()
-reclame_tekst4= reclame_tekst3.split()
-print(reclame_tekst4)
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el.upper())
-    else:
-        print(el.lower())
+    reclame_tekst= f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+    reclame_tekst2= reclame_tekst[:63]
+    reclame_tekst3= reclame_tekst2 .upper()
+    reclame_tekst4= reclame_tekst3.split()
+    print(reclame_tekst4)
+    for el in reclame_tekst4:
+        if len(el) >= 5:
+            print(el.upper())
+        else:
+            print(el.lower())
 
 print_aanbieding()
 decoreer("Aanbieding")
