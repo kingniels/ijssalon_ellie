6ef438f081b0eab674cafaf8efaaa0dea11178c2
Niels van den Hoven <nielsvdhoven@gmail.com>
2023-11-20 22:00:41 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,11 +1,14 @@
-mijn_dictionairy = {
-    "aardbei" : 3,
-    "vanille" : 4.0,
-    "chocolade" : 5
-}
-aanbieding = mijn_dictionairy["vanille"] * 0.8
-reclame_tekst= f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
-reclame_tekst2= reclame_tekst[:62]
+from helper import decoreer
+
+def print_aanbieding() :
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
+    aanbieding = prijzen["aardbei"] * 0.8
+reclame_tekst= f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+reclame_tekst2= reclame_tekst[:63]
 reclame_tekst3= reclame_tekst2 .upper()
 reclame_tekst4= reclame_tekst3.split()
 print(reclame_tekst4)
@@ -14,3 +17,6 @@ for el in reclame_tekst4:
         print(el.upper())
     else:
         print(el.lower())
+
+print_aanbieding()
+decoreer("Aanbieding")
